c56475bfe60854ea4c66002efeaf92dba9783afd
100percentBAKA <1rn21cs011.adarshgs@gmail.com>
2024-12-08 13:40:25 +0530

    final setup

--- client/src/service/config.tsx
+++ client/src/service/config.tsx
@@ -1,6 +1,7 @@
 import { Platform } from "react-native";
 
-export const BASE_URL = "http://192.168.56.1:8000"
+export const BASE_URL =
+  "https://9e82-2402-e280-2117-556-e030-7ea1-c03f-2262.ngrok-free.app";
 
 export const SOCKET_URL =
-  Platform.OS === "ios" ? "ws://localhost:3000" : "ws://10.0.2.2:3000";
+  "ws://9e82-2402-e280-2117-556-e030-7ea1-c03f-2262.ngrok-free.app";
